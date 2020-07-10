2b1e5abe3978573a06d9e9fccdb39d2d4f145cbe
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-07-09 20:56:56 -0500

    Add pair sq function

--- scattering/scattering.py
+++ scattering/scattering.py
@@ -90,8 +90,125 @@ def structure_factor(trj, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False,
             try:
                 g_r = rdfs['{0}{1}'.format(e1, e2)]
             except KeyError:
-                pairs = top.select_pairs(selection1='element {}'.format(e1),
-                                         selection2='element {}'.format(e2))
+                if framewise_rdf:
+                    r, g_r = rdf_by_frame(trj,
+                                         pairs=pairs,
+                                         r_range=(0, L / 2),
+                                         bin_width=0.001)
+                else:
+                    r, g_r = md.compute_rdf(trj,
+                                            pairs=pairs,
+                                            r_range=(0, L / 2),
+                                            bin_width=0.001)
+                rdfs['{0}{1}'.format(e1, e2)] = g_r
+            integral = simps(r ** 2 * (g_r - 1) * np.sin(q * r) / (q * r), r)
+
+            if method == 'fz':
+                pre_factor = 4 * np.pi * rho
+                partial_sq = (integral*pre_factor) + 1
+                num += (x_a*f_a*x_b*f_b) * (partial_sq)
+        S[i] = (num/(denom**2))
+    return Q, S
+
+
+def structure_factor_pair(trj, pair, Q_range=(0.5, 50), n_points=1000, framewise_rdf=False, method='fz'):
+    """Compute the structure factor.
+
+    The consdered trajectory must include valid elements.
+
+    The computed structure factor is only valid for certain values of Q. The
+    lowest value of Q that can sufficiently be described by a box of
+    characteristic length `L` is `2 * pi / (L / 2)`.
+
+    Parameters
+    ----------
+    trj : mdtraj.Trajectory
+        A trajectory for which the structure factor is to be computed.
+    pair : list
+        List of pairs to consider in partial structure factor
+    Q_range : list or np.ndarray, default=(0.5, 50)
+        Minimum and maximum Values of the scattering vector, in `1/nm`, to be
+        consdered.
+    n_points : int, default=1000
+    framewise_rdf : boolean, default=False
+        If True, computes the rdf frame-by-frame. This can be useful for
+        managing memory in large systems.
+    method : string, optional, default='fz'
+        Formalism for calculating the structure-factor, default is Faber-Ziman.
+        See https://openscholarship.wustl.edu/etd/1358/ and http://isaacs.sourceforge.net/manual/page26_mn.html for details.
+
+    Returns
+    -------
+    Q : np.ndarray
+        The values of the scattering vector, in `1/nm`, that was considered.
+    S : np.ndarray
+        The structure factor of the trajectory
+
+    """
+    if method not in ['fz']:
+        raise ValueError('Invalid method `{}` is given.'
+                         '  The only method currently supported is `fz`.'.format(
+                             method))
+
+    rho = np.mean(trj.n_atoms / trj.unitcell_volumes)
+    L = np.min(trj.unitcell_lengths)
+
+    trj1 = trj.atom_slice(trj.topology.select('resname {}'.format(pair[0])))
+    trj2 = trj.atom_slice(trj.topology.select('resname {}'.format(pair[1])))
+
+    elements_i = set()
+    elements_j = set()
+    for atom in trj1.top.atoms:
+        elements_i.add(atom.element)
+    for atom in trj2.top.atoms:
+        elements_j.add(atom.element)
+
+    compositions_i = dict()
+    compositions_j = dict()
+    form_factors_i = dict()
+    form_factors_j = dict()
+    rdfs = dict()
+
+    Q = np.logspace(np.log10(Q_range[0]),
+                    np.log10(Q_range[1]),
+                    num=n_points)
+    S = np.zeros(shape=(len(Q)))
+
+    for elem in elements_i:
+        compositions_i[elem.symbol] = len(trj1.top.select('element {}'.format(elem.symbol)))/trj.n_atoms
+        form_factors_i[elem.symbol] = elem.atomic_number
+    for elem in elements_j:
+        compositions_j[elem.symbol] = len(trj2.top.select('element {}'.format(elem.symbol)))/trj.n_atoms
+        form_factors_j[elem.symbol] = elem.atomic_number
+
+    for i, q in enumerate(Q):
+        num = 0
+        denom = 0
+
+        for elem in elements_i:
+            denom += (compositions_i[elem.symbol] * form_factors_i[elem.symbol])
+        for elem in elements_j:
+            denom += (compositions_j[elem.symbol] * form_factors_j[elem.symbol])
+
+        for (elem1, elem2) in it.product(elements_i, elements_j):
+            e1 = elem1.symbol
+            e2 = elem2.symbol
+
+            f_a = form_factors_i[e1]
+            f_b = form_factors_j[e2]
+
+            x_a = compositions_i[e1]
+            x_b = compositions_j[e2]
+            
+            try:
+                g_r = rdfs['{0}{1}'.format(e1, e2)]
+            except KeyError:
+
+                selection1 = 'element {}'.format(e1)
+                selection2 = 'element {}'.format(e2)
+
+                pairs = trj.top.select_pairs(selection1=selection1,
+                                         selection2=selection2)
                 if framewise_rdf:
                     r, g_r = rdf_by_frame(trj,
                                          pairs=pairs,
